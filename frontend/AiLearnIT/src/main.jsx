811b358bc8f4d15f20990223d3b84ed56f20002c
Manav Thakur <manavthakur652@gmail.com>
2026-03-19 22:28:48 +0530

    feat: add Vercel configuration and .gitignore for environment variables

--- frontend/AiLearnIT/src/main.jsx
+++ frontend/AiLearnIT/src/main.jsx
@@ -4,12 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
 import { Toaster } from 'react-hot-toast'
-
+import { BrowserRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
+     <BrowserRouter>
       <Toaster position='top-right' toastOptions={{ duration: 3000 }} />
       <App />
+     </BrowserRouter>
     </AuthProvider>
   </StrictMode>
 )
