e0c9edf01be3156838472af4f1f97508a0176cdf
Manav Thakur <manavthakur652@gmail.com>
2026-03-20 00:56:07 +0530

    add routes of pages

--- frontend/AiLearnIT/src/App.jsx
+++ frontend/AiLearnIT/src/App.jsx
@@ -1,6 +1,20 @@
 // Router import hatao
 import { Routes, Route, Navigate } from 'react-router-dom'  // BrowserRouter as Router ❌
 import { useAuth } from './context/AuthContext'
+import LoginPage from './Pages/Auth/LoginPage'
+import RegisterPage from './Pages/Auth/RegisterPage'
+import NotFoundPage from './Pages/NotFoundPage'
+import { ProtectedRoute } from './components/auth/ProtectedRoute'
+
+import DashPage from "./Pages/Dahboard/DashPage"
+import DocListPage from "./Pages/Documents/DocListPage"
+import DocDetailPage from "./Pages/Documents/DocDetailPage"
+import FlashListPage from "./Pages/Flashcard/FlashListPage"
+import FlashcardPage from "./Pages/Flashcard/FlashcardPage"
+import QuizTakePage from "./Pages/Quizzes/QuzTakePage"
+import QuizResPage from "./Pages/Quizzes/QuzResPage"
+import ProfilePage from "./Pages/Profile/ProfilePage"
+import { AppLayout } from './components/layout/AppLayout'
 
 export default function App() {
   const { isAuthenticated, loading } = useAuth()
